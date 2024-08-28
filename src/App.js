71f8a07eca4ad3a57b24646333a1700716fb5b7d
JordaineGR <jordaine.g.r@gmail.com>
2024-08-28 14:13:44 -0400

    Added GitHub repo URL

--- src/App.js
+++ src/App.js
@@ -2,5 +2,18 @@ import React from "react";
 import "./App.css";
 
 export default function App() {
-  return <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+      Weather App
+      <footer>
+        This project was coded by j'ordaine guiseppi-ramos and is
+        <a
+          href="https://github.com/JordaineGR/react-weather-app"
+          target="_blank"
+        >
+          open-sourced on GitHub
+        </a>
+      </footer>
+    </div>
+  );
 }
