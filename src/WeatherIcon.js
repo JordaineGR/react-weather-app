8a688232a00cdb475d5f02efb5b2cc9ab9fbdb4d
JordaineGR <jordaine.g.r@gmail.com>
2024-09-07 20:17:19 -0400

    Created Weather Temperature to convert celsius to fahrenheit

--- src/WeatherIcon.js
+++ src/WeatherIcon.js
@@ -3,30 +3,30 @@ import ReactAnimatedWeather from "react-animated-weather";
 
 export default function WeatherIcon(props) {
   const codeMapping = {
-    "clear-sky-day": "Clear_sky",
-    "clear-sky-night": "Clear_sky",
-    "few-clouds-day": "Few_clouds",
-    "few-clouds-night": "Few_clouds",
-    "scattered-clouds-day": "Scattered_clouds",
-    "scattered-clouds-night": "Scattered_clouds",
-    "broken-clouds-day": "Broken_clouds",
-    "broken-clouds-night": "Broken_clouds",
-    "shower-rain-day": "Shower_rain",
-    "shower-rain-night": "Shower_rain",
-    "rain-day": "Rain",
-    "rain-night": "Rain",
-    "thunderstorm-day": "Thunderstorm",
-    "thunderstorm-night": "Thunderstorm",
-    "snow-day": "Snow",
-    "snow-night": "Snow",
-    "mist-day": "Mist",
-    "mist-night": "Mist",
+    "clear-sky-day": "CLEAR_DAY",
+    "clear-sky-night": "CLEAR_NIGHT",
+    "few-clouds-day": "PARTLY_CLOUDY_DAY",
+    "few-clouds-night": "PARTLY_CLOUDY_NIGHT",
+    "scattered-clouds-day": "PARTLY_CLOUDY_DAY",
+    "scattered-clouds-night": "PARTLY_CLOUDY_NIGHT",
+    "broken-clouds-day": "CLOUDY",
+    "broken-clouds-night": "CLOUDY",
+    "shower-rain-day": "RAIN",
+    "shower-rain-night": "RAIN",
+    "rain-day": "RAIN",
+    "rain-night": "RAIN",
+    "thunderstorm-day": "RAIN",
+    "thunderstorm-night": "RAIN",
+    "snow-day": "SNOW",
+    "snow-night": "SNOW",
+    "mist-day": "FOG",
+    "mist-night": "FOG",
   };
   return (
     <ReactAnimatedWeather
       icon={codeMapping[props.code]}
-      color="red"
-      size={20}
+      color="black"
+      size={65}
       animate={true}
     />
   );
