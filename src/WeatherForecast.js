bcc33bf88fa8fe29c5f33288c0b0ba20a7cfe4ac
JordaineGR <jordaine.g.r@gmail.com>
2024-09-17 14:36:19 -0400

    Revert "updated WeatherForecast.js"
    This reverts commit a9e74f1076c44a48eceb26277226bc354a06d54d.

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -12,8 +12,8 @@ export default function WeatherForecast(props) {
   }, [props.coordinates]);
 
   function handleResponse(response) {
-    setLoaded(true);
     setForecast(response.data.daily);
+    setLoaded(true);
   }
 
   if (loaded) {
