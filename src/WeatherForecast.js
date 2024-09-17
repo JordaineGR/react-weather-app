1d3fb514a92e499a656553f0c6ef452feda54b13
JordaineGR <jordaine.g.r@gmail.com>
2024-09-17 14:36:46 -0400

    Revert "Updated weather forecast to display 5 days"
    This reverts commit 2286ae6cd311a1c536016016a74075931d878a8c.

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import "./WeatherForecast.css";
 import axios from "axios";
 import WeatherForecastDay from "./WeatherForecastDay";
@@ -6,11 +6,6 @@ import WeatherForecastDay from "./WeatherForecastDay";
 export default function WeatherForecast(props) {
   let [loaded, setLoaded] = useState(false);
   let [forecast, setForecast] = useState(null);
-
-  useEffect(() => {
-    setLoaded(false);
-  }, [props.coordinates]);
-
   function handleResponse(response) {
     setForecast(response.data.daily);
     setLoaded(true);
@@ -20,22 +15,16 @@ export default function WeatherForecast(props) {
     return (
       <div className="WeatherForecast">
         <div className="row">
-          {forecast.map(function (dailyForecast, index) {
-            if (index < 5) {
-              return (
-                <div className="col">
-                  <WeatherForecastDay data={dailyForecast} />
-                </div>
-              );
-            }
-          })}
+          <div className="col">
+            <WeatherForecastDay data={forecast[0]} />
+          </div>
         </div>
       </div>
     );
   } else {
-    let apiKey = "f59of3bc0634b982da3taabda665c640";
-    let longitude = props.coordinates.longitude;
-    let latitude = props.coordinates.latitude;
+    let apiKey = " f59of3bc0634b982da3taabda665c640";
+    let longitude = props.coordinates.lon;
+    let latitude = props.coordinates.lat;
     let apiUrl = `https://api.shecodes.io/weather/v1/forecast?lat=${latitude}&lon=${longitude}&key=${apiKey}&units=metric`;
 
     axios.get(apiUrl).then(handleResponse);
